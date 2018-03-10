712cb34ee8975fd1bb6ed657787c4a12b9a1d5ba
Gnaneshb <gnanesh.bammidi320@gmail.com>
2018-03-10 20:04:22 +0530

    Add existing file

--- pytorchbasics.py
+++ pytorchbasics.py
@@ -14,4 +14,10 @@ print(x.grad)
 print(y.grad)
 						 
 ## Example 2 - linear regression
-             
+
+x = Variable(torch.randn(5,3))
+y = Variable(torch.randn(5,2))
+
+linear = nn.Linear(3,2)
+print('W: ', linear.weight)
+print('b: ',linear.bias)
