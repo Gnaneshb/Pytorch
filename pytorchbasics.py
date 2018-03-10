3bf185c21f3da97a200f2a548e2a1af1c46c9843
Gnaneshb <gnanesh.bammidi320@gmail.com>
2018-03-10 19:43:42 +0530

    Update pytorchbasics.py

--- pytorchbasics.py
+++ pytorchbasics.py
@@ -4,3 +4,14 @@ from torch.autograd import Variable
 import torch.nn as nn
 
 ## Example 1 - variable and grad 
+x = variable(torch.IntTorch([4],requires_grad = True)
+y = variable(torch.IntTorch([6],requires_grad = True)
+             
+z = y**2+x+1
+z.backward()
+
+print(x.grad)
+print(y.grad)
+						 
+## Example 2 - linear regression
+             
